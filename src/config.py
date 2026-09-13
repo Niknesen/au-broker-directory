59582a19fdfbf3e3ba7b0053a28d5976d774787e
DeepMineWorld <95085907+DeepMineWorld@users.noreply.github.com>
2026-09-13 15:11:28 +0800

    [analytics] add Google Tag gtag.js G-KVR945JRS2 across all pages

--- src/config.py
+++ src/config.py
@@ -24,6 +24,7 @@ CONTACT_EMAIL = "nick@aiex.team"
 
 # Analytics & Tracking Pixels
 GTM_CONTAINER_ID = "GTM-PNGJR452"
+GA4_MEASUREMENT_ID = "G-KVR945JRS2"
 META_PIXEL_ID = "1256510258585870"  # Nick pixel
 
 # Local dev server
